e5f95c187e57428c7a99d44cc5f1bbbdd4f86724
onesg <gabriel.seno@fatec.sp.gov.br>
2022-04-29 21:59:28 -0300

    Implementado menu tabs e página de login

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  Button
+} from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+{/* CONTEÚDO DA PÁGINA DE HOME */ }
 function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+      <Text>Página home</Text>
     </View>
   );
 }
 
-function SettingsScreen() {
+{/* CONTEÚDO DA PÁGINA DE LOGIN */ }
+function LoginScreen() {
   return (
+
+    /* INICIANDO A VIEW */
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+
+      {/* DESCRIÇÃO DA PÁGINA (COLOCAR ÍCONE DEPOIS) */}
+      <Text>Realizar Login</Text>
+
+      {/* CAMPO PARA E-MAIL */}
+      <TextInput
+        placeholder="E-mail"
+        placeholderTextColor="#313131"
+        style={styles.input}>
+      </TextInput>
+
+      {/* CAMPO PARA SENHA */}
+      <TextInput
+        placeholder="Senha"
+        placeholderTextColor="#313131"
+        style={styles.input}>
+      </TextInput>
+
+      {/* BOTÃO PARA REALIZAR LOGIN */}
+      <Button title="Logar" color="#ff6347" />
+
     </View>
+    /* TERMINANDO A VIEW */
+
   );
+
 }
 
+{/* *** */ }
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -30,28 +65,35 @@ export default function App() {
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-list-box' : 'ios-list';
+            if (route.name === 'Início') {
+              iconName = focused ? 'home-sharp' : 'home-outline';
+            } else if (route.name === 'Login') {
+              iconName = focused ? 'person' : 'person-outline';
             }
-
-            // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: 'tomato',
           tabBarInactiveTintColor: 'gray',
         })}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        {/* BOTÃO COM EMBLEMA (NOTIFICAÇÃO)
-        <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} />
-        */}
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Início" component={HomeScreen} />
+        <Tab.Screen name="Login" component={LoginScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
+
 }
+
+/* ESTILOS DOS COMPONENTES */
+const styles = StyleSheet.create({
+
+  /* CAMPOS DO FORMULÁRIO */
+  input: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#313131',
+    marginTop: 5,
+    width: '80%',
+    height: 50,
+  },
+
+})
